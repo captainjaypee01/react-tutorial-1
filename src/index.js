eac5b28254d80a4af01a8202c318752627b2efa8
John Paul Dala <jaypeedala31@gmail.com>
2022-05-02 23:15:48 +0800

    Different usage of rendering the components using functional or arrow function components

--- src/index.js
+++ src/index.js
@@ -1,17 +1,27 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+// Since in the index.html the id is root, we must get the reference from the id which is root
+// Now We need to import the ReactDom to render the component inside the element in index.html by root id
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// Always use capital case
+
+// we must return html in index js
+// function Greeting() {
+//   return (
+//     <div>
+//       <h4>I'm Jaypee!</h4>
+//     </div>
+//   );
+// }
+
+const Greeting = () => {
+  return React.createElement(
+    "div",
+    {},
+    React.createElement("h1", {}, "Hi I'm Jaypee")
+  );
+};
+
+// Always remember the closing tag for self closing tag if there is no closing tag.
+ReactDOM.render(<Greeting />, document.getElementById("root"));

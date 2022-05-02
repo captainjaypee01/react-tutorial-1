7de6dc8844f01502ee97703d91d3eb4682fa815c
John Paul Dala <jaypeedala31@gmail.com>
2022-05-02 23:29:54 +0800

    JSX Rules

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import ReactDOM from "react-dom/client";
 
 // Since in the index.html the id is root, we must get the reference from the id which is root
 // Now We need to import the ReactDom to render the component inside the element in index.html by root id
@@ -7,21 +7,38 @@ import ReactDOM from "react-dom";
 // Always use capital case
 
 // we must return html in index js
-// function Greeting() {
-//   return (
-//     <div>
-//       <h4>I'm Jaypee!</h4>
-//     </div>
-//   );
-// }
 
-const Greeting = () => {
-  return React.createElement(
-    "div",
-    {},
-    React.createElement("h1", {}, "Hi I'm Jaypee")
+// JSX Rules
+// - return single element
+// - div / section / article or Fragment
+// - use camelCase for html attribute
+// - className instead of class
+// - close every element
+// - formatting
+// - Follow semantics for better practice
+function Greeting() {
+  return (
+    <>
+      <div className="test">
+        <h4>I'm Jaypee!</h4>
+        <ul>
+          <li>
+            <a href="#">Click Me!</a>
+          </li>
+        </ul>
+      </div>
+    </>
   );
-};
+}
+
+// const Greeting = () => {
+//   return React.createElement(
+//     "div",
+//     {},
+//     React.createElement("h1", {}, "Hi I'm Jaypee")
+//   );
+// };
 
 // Always remember the closing tag for self closing tag if there is no closing tag.
-ReactDOM.render(<Greeting />, document.getElementById("root"));
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<Greeting />);
